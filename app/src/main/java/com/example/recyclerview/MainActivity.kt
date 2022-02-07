d2d19549c5cbedc96fa27ce7da88d2f2c220faf6
isilay-subasi <isil.subasi19@gmail.com>
2022-02-07 18:21:53 +0300

    RecuclerView&CardView

--- app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.recyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val arrayList = ArrayList<Model>()
+        arrayList.add(Model("Chips","Description chips",R.drawable.icon1))
+        arrayList.add(Model("Cake","Description cake",R.drawable.icon2))
+        arrayList.add(Model("Jam","Description jam",R.drawable.icon3))
+        arrayList.add(Model("Soup","Description soup",R.drawable.icon4))
+
+
+        val myAdapter= MyAdapter(arrayList,this)
+
+        recyclerView.layoutManager=LinearLayoutManager(this)
+        recyclerView.adapter=myAdapter
+
     }
 }
--- app/src/main/java/com/example/recyclerview/MyAdapter.kt
+++ app/src/main/java/com/example/recyclerview/MyAdapter.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.row.view.*
 
@@ -30,6 +31,24 @@ class MyAdapter (val arrayList: ArrayList<Model> , val context : Context) :
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.bindItems(arrayList[position])
+
+        holder.itemView.setOnClickListener {
+            if (position == 0){
+                Toast.makeText(context,"İcon1",Toast.LENGTH_SHORT).show()
+            }
+            if (position == 1){
+                Toast.makeText(context,"İcon2",Toast.LENGTH_SHORT).show()
+            }
+            if (position == 2){
+                Toast.makeText(context,"İcon3",Toast.LENGTH_SHORT).show()
+            }
+            if (position == 3){
+                Toast.makeText(context,"İcon4",Toast.LENGTH_SHORT).show()
+            }
+
+
+        }
+
     }
 
     override fun getItemCount(): Int {
